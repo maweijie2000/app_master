b21807ec71045430dfa25406cd8fb61cb9c0c54a
马伟杰 <ma.weijie@upex.co>
2021-08-31 22:17:12 +0800

    加入引用

--- app/src/main/java/com/jimes/app/MainActivity.kt
+++ app/src/main/java/com/jimes/app/MainActivity.kt
@@ -2,10 +2,13 @@ package com.jimes.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RestrictTo
+import com.gyf.immersionbar.ImmersionBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
